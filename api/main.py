23ec5350d059254e596277a42da00090edd28e7b
mateusz <m.kolomanski@yahoo.com>
2026-02-16 17:45:39 +0100

    chore: renamed api.py to main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from .system.router import router as system_router
 
-app = FastAPI()
+api = FastAPI()
 
-app.include_router(system_router)
+api.include_router(system_router)
 
-@app.get("/")
+@api.get("/")
 async def root():
     return {"message": "Hello World"}
 
