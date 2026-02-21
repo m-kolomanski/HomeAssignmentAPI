a971e15dd5ad8fb1bb6b00c8806e9c1bb730b19e
m-kolomanski <m.kolomanski@yahoo.com>
2026-02-21 16:45:06 +0100

    feat: endpoint for overwriting files

--- api/files/router.py
+++ api/files/router.py
@@ -2,13 +2,28 @@ from fastapi import APIRouter, UploadFile, Response
 from fastapi.responses import FileResponse
 import os
 
+FILE_STORAGE = "/app/userfiles"
+
 router = APIRouter(tags = ["files"])
+
+@router.get("/files")
+async def get_files():
+    return os.listdir(FILE_STORAGE)
+
+@router.get("/files/{filename}")
+async def get_file(filename: str):
+    file_path = f"{FILE_STORAGE}/{filename}"
+
+    if not os.path.exists(file_path):
+        return Response(status_code = 404)
+
+    return FileResponse(file_path)
 
 @router.post("/files")
 async def upload_files(file: UploadFile):
     contents = await file.read()
 
-    with open(f"/app/userfiles/{file.filename}", "wb") as f:
+    with open(f"{FILE_STORAGE}/{file.filename}", "wb") as f:
         f.write(contents)
 
     return {
@@ -17,10 +32,20 @@ async def upload_files(file: UploadFile):
         "size": len(contents)
     }
 
-@router.get("/files")
-async def get_files():
-    return os.listdir("/app/userfiles")
+@router.put("/files/{filename}")
+async def update_file(filename: str, file: UploadFile):
+    file_path = f"{FILE_STORAGE}/{filename}"
 
-@router.get("/files/{filename}")
-async def get_file(filename: str):
-    return FileResponse(f"/app/userfiles/{filename}")
+    if not os.path.exists(file_path):
+        return Response(status_code = 404)
+    
+    contents = await file.read()
+
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    return {
+        "filename": filename,
+        "content_type": file.content_type,
+        "size": len(contents)
+    }
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-
+import pytest
 from api.main import api
 
 client = TestClient(api)
@@ -22,3 +22,42 @@ def test_file_upload():
 def test_file_list():
     response = client.get("/files")
     assert response.json() == ["test_file.csv"]
+
+@pytest.mark.parametrize(
+    "filename,expected_code,expected_text",
+    [
+        ("test_file.csv", 200, "col1,col2,col3\nval1,val2,val3"),
+        ("invalid_file.csv", 404, "")
+    ]
+)
+def test_get_file(filename, expected_code, expected_text):
+    response = client.get(f"/files/{filename}")
+
+    assert response.status_code == expected_code
+    assert response.text == expected_text
+
+@pytest.mark.parametrize(
+    "filename,expected_code,expected_text",
+    [
+        ("test_file.csv", 200, "col1,col2,col3\nval1,val2,val3\nval4,val5,val6"),
+        ("invalid_file.csv", 404, None)
+    ]
+)
+def test_update_file(filename, expected_code, expected_text):
+    with open("tests/data/test_file2.csv", "rb") as f:
+        response = client.put(
+          f"/files/{filename}",
+          files = { "file": ("test_file.csv", f, "text/csv")}
+        )
+
+    assert response.status_code == expected_code
+
+    if response.status_code == 200:
+        assert response.json() == {
+            "filename": "test_file.csv",
+            "content_type": "text/csv",
+            "size": 44
+        }
+
+        file_response = client.get(f"/files/{filename}")
+        assert file_response.text == expected_text
