082242ffff021df341ca900f5fc1224a87622fa1
m-kolomanski <m.kolomanski@yahoo.com>
2026-03-22 12:47:20 +0100

    fix: fix tests and fixtures

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,3 +1,5 @@
+import shutil
+
 def test_file_upload__ok(client, generate_csv):
     test_file = generate_csv()
 
@@ -14,8 +16,9 @@ def test_file_upload__ok(client, generate_csv):
             "size": 65
         }
     
-def test_file_upload__file_exists(client, generate_csv):
+def test_file_upload__file_exists(file_storage, client, generate_csv):
     test_file = generate_csv()
+    shutil.copy(test_file, file_storage)
 
     with open(test_file, "rb") as f:
         response = client.post(
@@ -36,11 +39,18 @@ def test_file_upload__invalid_mime(client, generate_csv):
 
     assert response.status_code == 415
 
-def test_file_list(client):
+def test_file_list(file_storage, client, generate_csv):
+    test_file = generate_csv()
+    shutil.copy(test_file, file_storage)
+
     response = client.get("/files")
+    assert response.status_code == 200
     assert response.json() == ["test_file.csv"]
 
-def test_get_file__ok(client):
+def test_get_file__ok(file_storage, client, generate_csv):
+    test_file = generate_csv()
+    shutil.copy(test_file, file_storage)
+
     response = client.get("/files/test_file.csv")
 
     assert response.status_code == 200
@@ -51,7 +61,10 @@ def test_get_file__missing(client):
 
     assert response.status_code == 404
 
-def test_update_file__ok(client, generate_csv):
+def test_update_file__ok(file_storage, client, generate_csv):
+    test_file_to_overwrite = generate_csv(name = "test_file.csv", cols = 1, rows = 3)
+    shutil.copy(test_file_to_overwrite, file_storage)
+
     test_file = generate_csv(name = "new_test_file.csv", cols = 5, rows = 1)
 
     with open(test_file, "rb") as f:
