07762b6398969e042c1b0d1009a2eb16aee32b16
m-kolomanski <m.kolomanski@yahoo.com>
2026-03-22 14:12:32 +0100

    feat: added database for storing file metadata

---
+++ backend/files/__init__.py
@@ -0,0 +1,2 @@
+from . import models
+from .router import router
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from .database import db_create
+
 from .system.router import router as system_router
 from .files.router import router as files_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_create()
+    yield
+
+app = FastAPI(lifespan = lifespan)
 
 app.include_router(system_router)
 app.include_router(files_router)
