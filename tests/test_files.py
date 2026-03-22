fd33281681460f1ca4e9da22d74ac8805db45f66
m-kolomanski <m.kolomanski@yahoo.com>
2026-03-22 12:56:38 +0100

    feat: extended file related tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,19 +1,29 @@
 import shutil
+import pytest
 
-def test_file_upload__ok(client, generate_csv):
-    test_file = generate_csv()
+@pytest.mark.parametrize(
+    ("filename", "ncol", "nrow", "size"),
+    [
+        ("test_file.csv", 3, 2, 65),
+        ("test_file_no_rows.csv", 3, 0, 17),
+        ("test_file_mid.csv", 5, 10, 429),
+        ("test_file_big.csv", 30, 100, 28899)
+    ]
+)
+def test_file_upload__ok(client, generate_csv, filename, ncol, nrow, size):
+    test_file = generate_csv(filename, ncol, nrow)
 
     with open(test_file, "rb") as f:
         response = client.post(
             "/files",
-            files = { "file": ("test_file.csv", f, "text/csv")}
+            files = { "file": (filename, f, "text/csv")}
         )
 
     assert response.status_code == 200
     assert response.json() == {
-            "filename": "test_file.csv",
+            "filename": filename,
             "content_type": "text/csv",
-            "size": 65
+            "size": size
         }
     
 def test_file_upload__file_exists(file_storage, client, generate_csv):
@@ -39,13 +49,22 @@ def test_file_upload__invalid_mime(client, generate_csv):
 
     assert response.status_code == 415
 
-def test_file_list(file_storage, client, generate_csv):
-    test_file = generate_csv()
-    shutil.copy(test_file, file_storage)
+@pytest.mark.parametrize(
+    ("filenames"),
+    [
+        (["single_file.csv"]),
+        (["first_file.csv", "second_file.csv"]),
+        ([f"file-{n}.csv" for n in range(20)])
+    ]
+)
+def test_file_list(file_storage, client, generate_csv, filenames):
+    for file in filenames:
+        test_file = generate_csv(file)
+        shutil.copy(test_file, file_storage)
 
     response = client.get("/files")
     assert response.status_code == 200
-    assert response.json() == ["test_file.csv"]
+    assert response.json().sort() == filenames.sort()
 
 def test_get_file__ok(file_storage, client, generate_csv):
     test_file = generate_csv()
