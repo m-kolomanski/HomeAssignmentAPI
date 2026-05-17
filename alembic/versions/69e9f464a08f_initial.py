cce9834d72e85a42fc1414e9522b181fb5956a96
m-kolomanski <m.kolomanski@yahoo.com>
2026-05-17 10:54:40 +0200

    feat: added uploaded_at

--- alembic/versions/69e9f464a08f_initial.py
+++ alembic/versions/69e9f464a08f_initial.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+import sqlmodel
 
 # revision identifiers, used by Alembic.
 revision: str = '69e9f464a08f'
--- backend/files/models.py
+++ backend/files/models.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, Field
-
+from datetime import datetime
 
 class File(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -8,3 +8,4 @@ class File(SQLModel, table=True):
     size: int
     ncol: int
     nrow: int
+    uploaded_at: datetime = Field(default_factory=lambda: datetime.now())
